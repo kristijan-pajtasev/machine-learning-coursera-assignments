8308cb68a34adfc4d41ebe5c13da6f380a113b1c
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-10-10 01:00:53 +0100

    logistical regression - assignment02

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,8 +17,16 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+shift_theta = theta(2:size(theta));
+theta_reg = [0;shift_theta];
+
+h =sigmoid(X * theta);
+J = (sum((-1) .* y .* log(h) - (1 .- y) .* log(1 .- h)) / m)  + (lambda / (2 * m))* sum(theta_reg .^ 2); 
 
 
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
+
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg); 
 
 
 
--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -3,13 +3,14 @@ function g = sigmoid(z)
 %   g = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
+sizes = size(z);
+g = zeros(sizes);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
+g = 1 ./ (1 .+ (e.^((-1) .* z)));
 
 
 
