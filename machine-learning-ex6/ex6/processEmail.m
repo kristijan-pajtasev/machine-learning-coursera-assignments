300b67dac785e9c2dc09ca33fb593458209b63a8
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-21 11:23:06 +0000

    ex6

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -99,7 +99,12 @@ while ~isempty(email_contents)
 
 
 
-
+  for i = 1:length(vocabList)
+        if strcmp(vocabList{i}, str)
+            word_indices = [word_indices; i];
+            break;
+        end
+    end
 
 
 
