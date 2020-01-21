aaef905fc2fc029d28d4acfcfa5158a7b973b062
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-21 11:34:21 +0000

    ex7

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -28,7 +28,17 @@ centroids = zeros(K, n);
 
 
 
+for i = 1:K
+    count = 0;
+    for j = 1:m
+        if idx(j) == i
+            count = count + 1;
+            centroids(i,:) = centroids(i,:) + X(j,:);
+        end
+    end
 
+    centroids(i,:) = centroids(i,:) / count;
+end
 
 
 
